d6f2dca7073d909df4b0f671c8aedbe8eba22b47
Valery Korzhavin <white.and.bushy@gmail.com>
2019-03-25 21:33:31 +0300

    fix eslint errors

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -6,10 +6,9 @@ const expectedFilePath = path.join(__dirname, '__fixtures__', 'diff');
 
 const yamlBeforeFilePath = path.join(__dirname, '__fixtures__', 'before.yaml');
 const yamlAfterFilePath = path.join(__dirname, '__fixtures__', 'after.yaml');
-  
+
 const jsonBeforeFilePath = path.join(__dirname, '__fixtures__', 'before.json');
 const jsonAfterFilePath = path.join(__dirname, '__fixtures__', 'after.json');
-  
 
 test('flat json diff', () => {
   const expected = fs.readFileSync(expectedFilePath, 'utf-8');
