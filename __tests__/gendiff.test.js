f330fafcd9f44485f4ba747907dec94183eb829a
Valery Korzhavin <white.and.bushy@gmail.com>
2019-03-25 19:20:20 +0300

    some refactoring

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,11 +2,11 @@ import fs from 'fs';
 import path from 'path';
 import genDiff from '../src';
 
-const firstFilePath = path.join(__dirname, '__fixtures__', 'before.json');
-const secondFilePath = path.join(__dirname, '__fixtures__', 'after.json');
-const expectedFilePath = path.join(__dirname, '__fixtures__', 'diff');
-const expected = fs.readFileSync(expectedFilePath, 'utf-8');
-
 test('flat json diff', () => {
+  const firstFilePath = path.join(__dirname, '__fixtures__', 'before.json');
+  const secondFilePath = path.join(__dirname, '__fixtures__', 'after.json');
+  const expectedFilePath = path.join(__dirname, '__fixtures__', 'diff');
+  const expected = fs.readFileSync(expectedFilePath, 'utf-8');
+
   expect(genDiff(firstFilePath, secondFilePath)).toBe(expected);
 });
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import _ from 'lodash';
+import _ from 'lodash/fp';
 import fs from 'fs';
 
 const genDiff = (firstFilePath, secondFilePath) => {
