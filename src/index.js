9512bb87dc8a740f4e37bdf878cc9f12ec502808
Valery Korzhavin <white.and.bushy@gmail.com>
2019-03-26 11:43:06 +0300

    fix cognitive complexity

--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ import fs from 'fs';
 import path from 'path';
 import parse from './parsers';
 
+const patterns = [
+  [
+    (key, obj1, obj2) => obj1[key] === obj2[key],
+    (key, obj1, obj2) => `    ${key}: ${obj2[key]}`,
+  ],
+  [
+    (key, obj1, obj2) => _.has(obj1, key) && _.has(obj2, key),
+    (key, obj1, obj2) => `  - ${key}: ${obj1[key]}\n  + ${key}: ${obj2[key]}`,
+  ],
+  [
+    (key, obj1) => _.has(obj1, key),
+    (key, obj1) => `  - ${key}: ${obj1[key]}`,
+  ],
+  [
+    (key, obj1, obj2) => _.has(obj2, key),
+    (key, obj1, obj2) => `  + ${key}: ${obj2[key]}`,
+  ],
+];
+
 const genDiff = (firstFilePath, secondFilePath) => {
   const firstFileData = fs.readFileSync(firstFilePath, 'utf8');
   const secondFileData = fs.readFileSync(secondFilePath, 'utf8');
@@ -14,16 +33,8 @@ const genDiff = (firstFilePath, secondFilePath) => {
   const objectsKeys = _.union(_.keys(obj1), _.keys(obj2));
 
   const diff = objectsKeys.map((key) => {
-    if (obj1[key] === obj2[key]) {
-      return `    ${key}: ${obj2[key]}`;
-    }
-    if (_.has(obj1, key) && _.has(obj2, key)) {
-      return `  - ${key}: ${obj1[key]}\n  + ${key}: ${obj2[key]}`;
-    }
-    if (_.has(obj1, key)) {
-      return `  - ${key}: ${obj1[key]}`;
-    }
-    return `  + ${key}: ${obj2[key]}`;
+    const [, action] = patterns.find(([check]) => check(key, obj1, obj2));
+    return action(key, obj1, obj2);
   });
   const output = `{\n${diff.join('\n')}\n}`;
 
