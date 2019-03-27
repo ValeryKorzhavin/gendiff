5a6aed2d11f91c273e8b4d6656e37002ad68cd17
Valery Korzhavin <white.and.bushy@gmail.com>
2019-03-27 12:33:04 +0300

    refactor simple render

--- src/renderers/renderer.js
+++ src/renderers/renderer.js
@@ -1,19 +1,31 @@
+const tab = depth => ' '.repeat(depth * 2);
+
 const typeMapping = {
   unchanged: ' ',
   added: '+',
   removed: '-',
   oldValue: '-',
   newValue: '+',
-  complex: '',
+  complex: ' ',
 };
 
-const render = diff => (
-  diff.reduce((result, { key, type, value }) => {
+const renderValue = (value, depth) => {
+  if (value instanceof Object) {
+    const entries = Object.keys(value).map(key => `${tab(depth + 4)}${key}: ${value[key]}`);
+    return ['{', ...entries, `${tab(depth + 2)}}`].join('\n');
+  }
+  return `${value}`;
+};
+
+const render = (diff, depth) => (
+  diff.map(({ key, type, value }) => {
     if (value instanceof Array) {
-      return { ...result, [`${typeMapping[type]}${key}`]: render(value) };
+      return `${tab(depth + 1)}`
+      + `${typeMapping[type]} ${key}: {\n${render(value, depth + 2).join('\n')}`
+      + `\n${tab(depth + 2)}}`;
     }
-    return { ...result, [`${typeMapping[type]} ${key}`]: value };
-  }, {})
+    return `${tab(depth + 1)}${typeMapping[type]} ${key}: ${renderValue(value, depth)}`;
+  })
 );
 
-export default diffAst => JSON.stringify(render(diffAst), ' ', 4).replace(/"/g, '').replace(/,/g, '');
+export default diffAst => `{\n${render(diffAst, 0)}\n}`.split(',').join('\n');
