c402eaa47d054e4211a91d013a9f509d69a64b46
Valery Korzhavin <white.and.bushy@gmail.com>
2019-03-27 11:17:58 +0300

    refactor render

--- src/renderers/renderer.js
+++ src/renderers/renderer.js
@@ -1,22 +1,19 @@
-import _ from 'lodash';
-
 const typeMapping = {
   unchanged: ' ',
   added: '+',
   removed: '-',
   oldValue: '-',
   newValue: '+',
-  complex: ' ',
+  complex: '',
 };
 
-const render = (diff) => {
-  const getJson = diff.reduce((result, { key, type, value }) => {
-    if (_.isArray(value)) {
-      return { ...result, [`${typeMapping[type]} ${key}`]: render(value) };
+const render = diff => (
+  diff.reduce((result, { key, type, value }) => {
+    if (value instanceof Array) {
+      return { ...result, [`${typeMapping[type]}${key}`]: render(value) };
     }
     return { ...result, [`${typeMapping[type]} ${key}`]: value };
-  }, {});
-  return getJson;
-};
+  }, {})
+);
 
-export default diffAst => JSON.stringify(render(diffAst), ' ', 2).replace(/"/g, '').replace(/,/g, '');
+export default diffAst => JSON.stringify(render(diffAst), ' ', 4).replace(/"/g, '').replace(/,/g, '');
